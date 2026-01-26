ff0235202d060eaee2f9fb8ec8e61af96a95c38b
Ymtkh21020289 <912112018@gse.okayama-c.ed.jp>
2026-01-26 10:44:21 +0900

    Update server.js

--- server.js
+++ server.js
@@ -160,6 +160,7 @@ io.on('connection', (socket) => {
         if (enemy && !enemy.isDead && player) {
             const sum = player.attackPower + data.damage
             enemy.hp -= sum;
+            console.log(`与えたダメージ:${sum}`);
             io.emit('enemyDamaged', { enemyId: enemyId, damage: sum });
 
             if (enemy.hp <= 0) {
