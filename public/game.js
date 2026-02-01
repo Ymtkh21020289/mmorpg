925018fddbf9895665e20d4b2c0c617bdae7ff04
Ymtkh21020289 <912112018@gse.okayama-c.ed.jp>
2026-02-02 00:22:27 +0900

    Update game.js

--- public/game.js
+++ public/game.js
@@ -319,7 +319,6 @@ function create() {
             const nameText = this.add.text(npc.x, npc.y - 30, npc.name, { fontSize: '14px', fill: '#fff' }).setOrigin(0.5);
             
             this.npcGroup.add(npcSprite);
-            this.npcGroup.add(nameText);
         }
     });
 
