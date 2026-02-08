7466f88c7d569d644b278693df51a2a0722eca65
Ymtkh21020289 <912112018@gse.okayama-c.ed.jp>
2026-02-09 01:00:34 +0900

    Update game.js

--- public/game.js
+++ public/game.js
@@ -1371,6 +1371,7 @@ function createSlot(scene, index, x, y, isHotbar) {
         if (!item) return; // 空なら何もしない
 
         const itemData = ITEMS[item.id];
+        console.log(`選択した武器のランク:`, item.rank);
         if(item.rank) {
             const rankId = item.rank;
             const rankData = RANKS[rankId];
