ddef904c98bc3773e3ee41b234d0824b4806b076
Ymtkh21020289 <912112018@gse.okayama-c.ed.jp>
2026-02-09 23:55:02 +0900

    Update game.js

--- public/game.js
+++ public/game.js
@@ -1387,11 +1387,13 @@ function createSlot(scene, index, x, y, isHotbar) {
     // ■ カーソルが乗ったとき（表示）
     bg.on('pointerover', (pointer) => {
         // 第2, 第3引数にブラウザ上の生座標を渡す
+        const item = scene.myInventory[index];
         showTooltip(itemData, pointer.event.clientX, pointer.event.clientY);
     });
 
     bg.on('pointermove', (pointer) => {
         // 動いている最中も追従させる
+        const item = scene.myInventory[index];
         updateTooltipPosition(pointer.event.clientX, pointer.event.clientY);
     });
 
