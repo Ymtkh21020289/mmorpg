5200e41c49ddbad08d3d96f5916fb464fec2b380
Ymtkh21020289 <912112018@gse.okayama-c.ed.jp>
2026-02-09 22:21:13 +0900

    Update game.js

--- public/game.js
+++ public/game.js
@@ -20,6 +20,7 @@ let otherPlayers;
 let map;
 let layer;
 let mapReady = false; // ★重要：マップ読み込み完了フラグ
+let mapmake = false;
 
 function preload() {
     this.load.image('tiles', 'assets/tiles.png');
@@ -158,13 +159,14 @@ function create() {
         }
     };
 
-    // 初期マップ作成
-    this.createMap('town');
-
     // --- Socket イベント ---
     this.socket.on('currentPlayers', function (players) {
         Object.keys(players).forEach(function (id) {
             if (players[id].playerId === self.socket.id) {
+                if(!mapmake) {
+                    this.createMap(players[id].room);
+                    mapmake = true;
+                }
                 addPlayer(self, players[id]);
             } else {
                 addOtherPlayers(self, players[id]);
