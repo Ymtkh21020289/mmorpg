7d8a93f8b0b31ee91981c7b5ceff75c8732489c2
Ymtkh21020289 <912112018@gse.okayama-c.ed.jp>
2026-02-12 12:00:24 +0900

    Update server.js

--- server.js
+++ server.js
@@ -98,7 +98,7 @@ const spawners = [
     // スライムの群れ（Townの左上、5匹、半径100pxに散らばる）
     { type: 'slime',   x: 140, y: 200, count: 5, radius: 75, room: 'adventure', spriteKey: 'slimeSprite' },
     // ウルフの群れ（Adventureマップ、3匹）
-    { type: 'wolf',    x: 496, y: 176, count: 3, radius: 100, room: 'adventure', spriteKey: 'wolfSprite'},
+    { type: 'wolf',    x: 496, y: 176, count: 2, radius: 100, room: 'adventure', spriteKey: 'wolfSprite'},
     { type: 'wolf',    x: 538, y: 538, count: 2, radius: 80, room: '1021', spriteKey: 'wolfSprite'},
     { type: 'wolf',    x: 538, y: 272, count: 2, radius: 80, room: '1021', spriteKey: 'wolfSprite'},
     { type: 'slime',    x: 1024, y: 256, count: 4, radius: 96, room: '1021', spriteKey: 'slimeSprite'},
