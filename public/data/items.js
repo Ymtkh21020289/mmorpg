b11433dd22c3329cf68c86a4735b59a89d300487
Ymtkh21020289 <912112018@gse.okayama-c.ed.jp>
2026-04-23 17:29:30 +0900

    Update items.js

--- public/data/items.js
+++ public/data/items.js
@@ -75,11 +75,6 @@ const RECIPES = [
         id: 'slime_sword_alpha',
         materials: { 'kingSlime_heart': 1, 'slime_sword': 1 },
         cost: 10000
-    },
-    {
-        id: 'mathili_gun',
-        materials: { 'kingSlime_heart': 10},
-        cost: 10000
     }
 ];
 
